c6c5abe56454f45d6c315a2647e6d9fa195ba6e4
dhanasekarp <126143349+dhanasekarp03@users.noreply.github.com>
2023-12-11 11:20:47 +0530

    Update axi_base_test.sv

--- test/axi_base_test.sv
+++ test/axi_base_test.sv
@@ -1,29 +1,29 @@
 
 //--------------------------------------------------------------------------------------------
-// Class: axi4_base_test
-// axi4_base test has the test scenarios for testbench 
+// Class: axi_base_test
+// axi_base test has the test scenarios for testbench 
 // Sequences are created and started in the test
 //--------------------------------------------------------------------------------------------
 
-class axi4_base_test extends uvm_test;
+class axi_base_test extends uvm_test;
 
    // factory registration  
-  `uvm_component_utils(axi4_base_test)
+  `uvm_component_utils(axi_base_test)
 
-  // Variable: axi4_env_h
+  // Variable: axi_env_h
   // Handle for environment 
-  axi4_env axi4_env_h;
+  axi_env axi4_env_h;
 
 //--------------------------------------------------------------------------------------------
 // Construct: new
 //  Initializes class object
 //
 // Parameters:
-//  name - axi4_base_test
+//  name - axi_base_test
 //  parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
 
-function axi4_base_test::new(string name = "axi4_base_test",uvm_component parent = null);
+function axi_base_test::new(string name = "axi4_base_test",uvm_component parent = null);
   super.new(name, parent);
 endfunction : new
 
@@ -35,11 +35,11 @@ endfunction : new
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
 
-function void axi4_base_test::build_phase(uvm_phase phase);
+function void axi_base_test::build_phase(uvm_phase phase);
   super.build_phase(phase);
   
   // Create the environment
-  axi4_env_h = axi4_env::type_id::create("axi4_env_h",this);
+  axi_env_h = axi4_env::type_id::create("axi4_env_h",this);
 endfunction : build_phase
 
 //--------------------------------------------------------------------------------------------
@@ -50,9 +50,9 @@ endfunction : build_phase
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
 
-task axi4_base_test::run_phase(uvm_phase phase);
+task axi_base_test::run_phase(uvm_phase phase);
 
-  phase.raise_objection(this, "axi4_base_test");
+  phase.raise_objection(this, "axi_base_test");
 
   `uvm_info(get_type_name(), $sformatf("Inside BASE_TEST"), UVM_NONE);
   super.run_phase(phase);
@@ -63,7 +63,4 @@ task axi4_base_test::run_phase(uvm_phase phase);
 endtask : run_phase
 
 
-endclass : axi4_base_test
-
-
-
+endclass : axi_base_test
