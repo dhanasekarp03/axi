bc5e54c651c91f4cca3ee313b410ffb238aa3922
dhanasekarp <126143349+dhanasekarp03@users.noreply.github.com>
2023-12-11 11:34:47 +0530

    Update axi_master_write_16b_test.sv

--- test/axi_master_write_16b_test.sv
+++ test/axi_master_write_16b_test.sv
@@ -2,7 +2,7 @@
 // Class: axi_master_write_16b_test
 //  Extends the base test and starts the sequence of 16 bit write
 //--------------------------------------------------------------------------------------------
-class axi_master_write_16b_test extends apb_base_test;
+class axi_master_write_16b_test extends axi_base_test;
   `uvm_component_utils(axi_master_write_16b_test)
   
   //Variable: axi_master_write_16b_transfer_h
